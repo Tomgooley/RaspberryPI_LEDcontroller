c6cf285a04bdfd17685e0626337f0d732162063c
Tomgooley <tom.gooley1@gmail.com>
2024-08-06 15:50:36 +1000

    Update john_test1.py
    changed to set all leds to white and script uses lgpio.gpio_sleep() for more precise timing and includes error handling to ensure the GPIO is properly released in case of an error or interruption.

--- john_test1.py
+++ john_test1.py
@@ -5,12 +5,12 @@ import time
 LED_PIN = 21  # GPIO pin connected to the data pin of the WS2812B
 NUM_LEDS = 300  # Number of LEDs in the strip
 
-# Timing constants
-T0H = 0.0000004  # 0.4us
-T0L = 0.00000085  # 0.85us
-T1H = 0.0000008  # 0.8us
-T1L = 0.00000045  # 0.45us
-RESET_TIME = 0.00005  # Reset signal low for 50us
+# Timing constants (in nanoseconds)
+T0H = 400  # 0.4us
+T0L = 850  # 0.85us
+T1H = 800  # 0.8us
+T1L = 450  # 0.45us
+RESET_TIME = 50000  # Reset signal low for 50us
 
 # Setup GPIO
 h = lgpio.gpiochip_open(0)
@@ -19,14 +19,14 @@ lgpio.gpio_claim_output(h, LED_PIN)
 def send_bit(bit):
     if bit == 1:
         lgpio.gpio_write(h, LED_PIN, 1)
-        time.sleep(T1H)
+        lgpio.gpio_sleep(h, 0, T1H)
         lgpio.gpio_write(h, LED_PIN, 0)
-        time.sleep(T1L)
+        lgpio.gpio_sleep(h, 0, T1L)
     else:
         lgpio.gpio_write(h, LED_PIN, 1)
-        time.sleep(T0H)
+        lgpio.gpio_sleep(h, 0, T0H)
         lgpio.gpio_write(h, LED_PIN, 0)
-        time.sleep(T0L)
+        lgpio.gpio_sleep(h, 0, T0L)
 
 def send_byte(byte):
     for i in range(8):
@@ -39,27 +39,23 @@ def send_color(g, r, b):
 
 def reset():
     lgpio.gpio_write(h, LED_PIN, 0)
-    time.sleep(RESET_TIME)
+    lgpio.gpio_sleep(h, 0, RESET_TIME)
 
 # Main function
 def set_led_color(r, g, b):
-    send_color(g, r, b)
+    for _ in range(NUM_LEDS):
+        send_color(g, r, b)
     reset()
 
 if __name__ == "__main__":
     try:
         while True:
-            # Red
-            set_led_color(255, 0, 0)
-            print("Setting LED RED")
-            time.sleep(1)
-            # Green
-            set_led_color(0, 255, 0)
-            print("Setting LED GREEN")
-            time.sleep(1)
-            # Blue
-            set_led_color(0, 0, 255)
-            print("Setting LED BLUE")
+            # White at 20% brightness
+            set_led_color(51, 51, 51)
+            print("Setting LED WHITE at 20% brightness")
             time.sleep(1)
     except KeyboardInterrupt:
+        lgpio.gpiochip_close(h)
+    except Exception as e:
+        print(f"An error occurred: {e}")
         lgpio.gpiochip_close(h)
