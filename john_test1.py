eb71070da8b17890b028906a0843cdc5e2e602d6
Tomgooley <tom.gooley1@gmail.com>
2024-08-06 15:57:10 +1000

    Update john_test1.py
    This script uses time.sleep for timing, which should work better with the lgpio library.

--- john_test1.py
+++ john_test1.py
@@ -5,12 +5,12 @@ import time
 LED_PIN = 21  # GPIO pin connected to the data pin of the WS2812B
 NUM_LEDS = 300  # Number of LEDs in the strip
 
-# Timing constants (in nanoseconds)
-T0H = 400  # 0.4us
-T0L = 850  # 0.85us
-T1H = 800  # 0.8us
-T1L = 450  # 0.45us
-RESET_TIME = 50000  # Reset signal low for 50us
+# Timing constants (in seconds)
+T0H = 0.0000004  # 0.4us
+T0L = 0.00000085  # 0.85us
+T1H = 0.0000008  # 0.8us
+T1L = 0.00000045  # 0.45us
+RESET_TIME = 0.00005  # Reset signal low for 50us
 
 # Setup GPIO
 h = lgpio.gpiochip_open(0)
@@ -19,14 +19,14 @@ lgpio.gpio_claim_output(h, LED_PIN)
 def send_bit(bit):
     if bit == 1:
         lgpio.gpio_write(h, LED_PIN, 1)
-        lgpio.gpio_sleep(h, 0, T1H)
+        time.sleep(T1H)
         lgpio.gpio_write(h, LED_PIN, 0)
-        lgpio.gpio_sleep(h, 0, T1L)
+        time.sleep(T1L)
     else:
         lgpio.gpio_write(h, LED_PIN, 1)
-        lgpio.gpio_sleep(h, 0, T0H)
+        time.sleep(T0H)
         lgpio.gpio_write(h, LED_PIN, 0)
-        lgpio.gpio_sleep(h, 0, T0L)
+        time.sleep(T0L)
 
 def send_byte(byte):
     for i in range(8):
@@ -39,7 +39,7 @@ def send_color(g, r, b):
 
 def reset():
     lgpio.gpio_write(h, LED_PIN, 0)
-    lgpio.gpio_sleep(h, 0, RESET_TIME)
+    time.sleep(RESET_TIME)
 
 # Main function
 def set_led_color(r, g, b):
